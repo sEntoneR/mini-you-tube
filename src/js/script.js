fd26d2aa0b1d35737528319cab285d68ab64d5f7
sEntoneR <alexanderkiryushkin@yandex.ru>
2019-04-10 15:17:04 +0300

    second commit

--- src/js/script.js
+++ src/js/script.js
@@ -101,3 +101,17 @@ const data = [
         }, 10);
      }
  });
+
+ function sliceTitle() {
+     document.querySelectorAll('.videos__item-descr').forEach(item => {
+        item.textContent.trim();
+
+        if (item.textContent.length < 100){
+            return;
+        } else {
+            const str = item.textContent.slice(0, 101) + "...";
+            item.textContent = str;
+        }
+     });
+ }
+ sliceTitle();
